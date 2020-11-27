dbea88037ad50f5c88efc88e5916f64997199829
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-11-27 16:30:52 +0100

    add controllers for get all

--- api/playlists/controllers.js
+++ api/playlists/controllers.js
@@ -1,27 +1,39 @@
 const db = require('../db-connection');
 
 const controllers = {
-  getAll: (req, res) => {
+    getAll: (req, res) => {
 
-    const sql = `SELECT * FROM playlists`;
+        const sql = `SELECT * FROM playlists`;
 
-    db.all(sql, (err, rows) => {
-      if (err) {
-        res.status(400).json({ "error": err.message });
-        return;
-      }
+        db.all(sql, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
 
-      res.json(rows)
-    });
-  },
-  getOne: (req, res) => { },
-  create: (req, res) => {
-    // read row data from body
-  },
-  update: (req, res) => {
-    // read row data from body
-  },
-  delete: (req, res) => { }
+            res.json(rows)
+        });
+    },
+    getOne: (req, res) => {
+        const sql = "select * from playlists where PlaylistId = ?"
+        const params = [req.params.id];
+        console.log(params)
+        db.get(sql, params, (err, row) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+
+            res.json(row)
+        })
+    },
+    create: (req, res) => {
+        // read row data from body
+    },
+    update: (req, res) => {
+        // read row data from body
+    },
+    delete: (req, res) => {}
 }
 
 module.exports = controllers;
